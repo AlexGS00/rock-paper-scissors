3fc7b875bc99fb2ea8659d71481a830fa00dd2b7
AlexC <acortojan@gmail.com>
2025-01-03 12:07:06 +0200

    implement computerChoice function

--- game.js
+++ game.js
@@ -11,5 +11,16 @@ function humanChoice(){
 }
 
 function computerChoice(){
+    let remainder = (Math.random * 100) % 3;
     
+    switch (remainder){
+        case 0:
+            return "rock";
+
+        case 1:
+            return "papper";
+        
+        case 2: 
+        return "scissors";
+    }
 }
